e07766dd8ecc18a22f45793ec27c40d00c70b739
nmkiely <nmkiely@ncsu.edu>
2017-01-20 08:57:20 -0500

    Refactor vectorLists

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc -Wall -std=c99
 
 all: c/LinkedLists/bin/linkedLists c/LinkedLists/bin/sortedLinkedLists \
-	c/Vectors/bin/vectorLists
+	c/Vectors/bin/vectorLists c/Vectors/bin/sortedVectorLists
 
 c/LinkedLists/bin/linkedLists: c/LinkedLists/linkedLists.c
 	$(CC) c/LinkedLists/linkedLists.c -o c/LinkedLists/bin/linkedLists
@@ -11,3 +11,6 @@ c/LinkedLists/bin/sortedLinkedLists: c/LinkedLists/sortedLinkedLists.c
 
 c/Vectors/bin/vectorLists: c/Vectors/vectorLists.c
 	$(CC) c/Vectors/vectorLists.c -o c/Vectors/bin/vectorLists
+
+c/Vectors/bin/sortedVectorLists: c/Vectors/sortedVectorLists.c
+	$(CC) c/Vectors/sortedVectorLists.c -o c/Vectors/bin/sortedVectorLists
--- c/Vectors/vectorLists.c
+++ c/Vectors/vectorLists.c
@@ -1,5 +1,5 @@
 /**
- * Demonstrates basic linked lists
+ * Demonstrates basic dynamic arrays
  * Reads commands from stdin, where each command is in the form:
  *  + <word>: Adds word to linked list
  *  - <word>: removes word from linked list
@@ -13,88 +13,157 @@
 
 #define MIN_SIZE 4
 
+
+/** Dynamic array data structure */
+typedef struct {
+  int size;    // Allocated size
+  int len;     // Number of items stored
+  char **list; // List
+} Vector;
+
+/** Reads input and stores in references to variables */
+bool fetchInput(size_t *bufSize, char **buf, char *cmd){
+  char *line = NULL;
+  *bufSize = 0;
+
+  // Read next line
+  ssize_t ret = getline(&line, bufSize, stdin);
+  if (ret <= 0)
+    return false;
+
+  // Copy raw input into new buffer; strip first character and newlines
+  *buf = malloc(*bufSize);
+
+  int i;
+  for (i=1; i<*bufSize && line[i]!='\0' && line[i]!='\n'; i++)
+    (*buf)[i-1] = line[i];
+  (*buf)[i-1] = '\0';
+
+  // Get command from original line buffer and free it
+  *cmd = line[0];
+  free(line);
+
+  return true;
+}
+
+/** Vector functions */
+void initializeVector(Vector *v);
+void describeVector(Vector *v);
+void updateVectorSize(Vector *v, bool grow);
+void freeVector(Vector *v);
+
+void vectorAdd(Vector *v, char *buf);
+void vectorRemove(Vector *v, char *buf);
+
+
 int main(int argc, char *argv[]){
-  int listSize = MIN_SIZE; // Number of allocated spaces in list
-  int listLen = 0; // Number of items stored in list
-  char **vectorList = malloc(listSize * sizeof(char *));
+  Vector vector;
+  initializeVector(&vector);
 
   while (true){
-    size_t bufSize = 0; // Buffer size
-    char *line = NULL; // Line input
-    ssize_t ret = getline(&line, &bufSize, stdin);
+    size_t bufSize;
+    char *buf;
+    char cmd;
 
     // Parse line into copy buffer
-    if (ret <= 0)
+    if (!fetchInput(&bufSize, &buf, &cmd))
       break;
-    char *buf = malloc(bufSize);
-
-    int i;
-    for (i=1; i<bufSize && line[i]!='\0' && line[i]!='\n'; i++)
-      buf[i-1] = line[i];
-    buf[i-1] = '\0';
-
-    // Get command from original line buffer and free it
-    char cmd = line[0];
-    free(line);
 
     switch (cmd){
     case '?':
-      // Query vector size
-      printf("Vector size: %d\t# items: %d\n", listSize, listLen);
-      free(buf);
+      // Print vector
+      describeVector(&vector);
       break;
 
     case '+':
-      // Add node
-      if (listLen+1 >= listSize){
-        // Resize vector
-        listSize *= 2;
-        vectorList = realloc(vectorList, listSize*sizeof(char *));
-      }
-      vectorList[listLen] = buf;
-      listLen += 1;
+      // Insert
+      vectorAdd(&vector, buf);
+      buf = NULL; // Buffer belongs to vector now
       break;
 
     case '-':
       // Free node
-      for (i=0; i<listLen; i++){
-        if (!strcmp(vectorList[i], buf)){
-          // Match found, delete string
-          free(vectorList[i]);
-          listLen -= 1;
-          break;
-        }
-      }
-      // Move nodes back
-      for (;i<=listLen; i++){
-        if (i+1 < listSize){
-          vectorList[i] = vectorList[i+1];
-        } else {
-          // Handle edge case where vector list is full
-          vectorList[i] = NULL;
-        }
-      }
-      free(buf);
-
-      // Check if vector needs to shrink
-      if (listSize>MIN_SIZE && 1+(listSize/4)>listLen){
-        listSize /= 2;
-        vectorList = realloc(vectorList, listSize*sizeof(char *));
-      }
+      vectorRemove(&vector, buf);
       break;
 
     default:
       fprintf(stderr, "Error: Line should start with + or -\n");
-      free(buf);
     }
-  }
 
+    if (buf)
+      free(buf);
+  }
 
   // Print and free vector list
-  printf("Vector size: %d\t# items: %d\n", listSize, listLen);
-  for (int i=0; i<listLen; i++){
-    printf("  ::: %s\n", vectorList[i]);
-    free(vectorList[i]);
+  describeVector(&vector);
+  freeVector(&vector);
+}
+
+
+void initializeVector(Vector *v){
+  v->size = MIN_SIZE;
+  v->len = 0;
+  v->list = malloc(v->size * sizeof(char *));
+}
+
+
+void updateVectorSize(Vector *v, bool grow){
+  if (grow){
+    // Adding data, check if buffer needs to grow
+    if (v->len+1 >= v->size){
+      v->size *= 2;
+      v->list = realloc(v->list, v->size*sizeof(char *));
+    }
+
+  } else {
+    // Deleting data, check if buffer needs to shrink
+    if (v->size>MIN_SIZE && 1+(v->size/4)>v->len){
+      v->size /= 2;
+      v->list = realloc(v->list, v->size*sizeof(char *));
+    }
   }
-  free(vectorList);
+}
+
+
+void describeVector(Vector *v){
+  printf("Vector size: %d\t# items: %d\n", v->size, v->len);
+}
+
+
+void freeVector(Vector *v){
+  for (int i=0; i < v->len; i++){
+    printf("  ::: %s\n", v->list[i]);
+    free(v->list[i]);
+  }
+  free(v->list);
+}
+
+
+void vectorAdd(Vector *v, char *buf){
+  updateVectorSize(v, true);
+  v->list[v->len] = buf;
+  v->len += 1;
+}
+
+
+void vectorRemove(Vector *v, char *buf){
+  int i;
+  for (i=0; i < v->len; i++){
+    if (!strcmp(v->list[i], buf)){
+      // Match found, delete string
+      free(v->list[i]);
+      v->len -= 1;
+      break;
+    }
+  }
+  // Move nodes back
+  for (; i<= v->len; i++){
+    if (i+1 < v->size){
+      v->list[i] = v->list[i+1];
+    } else {
+      // Handle edge case where vector list is full
+      v->list[i] = NULL;
+    }
+  }
+  updateVectorSize(v, true);
 }
